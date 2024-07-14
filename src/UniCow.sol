d5efcf0d4ac929e67b81d46cc041de27360979f7
chinmaygopal931 <chinmayg015@gmail.com>
2024-07-14 15:08:05 -0700

    chore: fix compile issues

--- src/UniCow.sol
+++ src/UniCow.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "forge-std/Test.sol";
-
-// import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {BaseHook} from "./forks/BaseHook.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {IHooks} from "v4-core/interfaces/IHooks.sol";
-import {Currency} from "v4-core/types/Currency.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {SafeCast} from "v4-core/libraries/SafeCast.sol";
@@ -16,33 +13,61 @@ import {BalanceDelta, BalanceDeltaLibrary} from "v4-core/types/BalanceDelta.sol"
 import {BeforeSwapDelta, toBeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/types/BeforeSwapDelta.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
 import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
-import {console} from "forge-std/console.sol";
-import {UniswapV4ERC20} from "v4-periphery/libraries/UniswapV4ERC20.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {IERC20Metadata} from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
-import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
-import {LibMulticaller} from "../lib/multicaller/src/LibMulticaller.sol";
+import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
+import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
 
 contract UniCow is BaseHook {
     using SafeCast for uint256;
     using PoolIdLibrary for PoolKey;
-    using Currency for Currency;
-
-    event OrderPartiallyFilled(
-        PoolId indexed poolId, address indexed owner, uint256 filledAmount, uint256 remainingAmount, bool isZeroForOne
-    );
-    event OrderCancelled(PoolId indexed poolId, address indexed owner, uint256 cancelledAmount, bool isZeroForOne);
+    using LPFeeLibrary for uint24;
+    using CurrencyLibrary for Currency;
+    using CurrencySettler for Currency;
+    using StateLibrary for IPoolManager;
 
     struct OrderObject {
         uint256 minPrice;
         uint256 deadline;
         uint256 amount;
-        bool isZeroForOne; // true if swapping token0 for token1
+        bool isZeroForOne;
     }
 
     mapping(PoolId => mapping(address => OrderObject)) public orders;
-    mapping(PoolId => mapping(address => uint256)) public userBalances0;
-    mapping(PoolId => mapping(address => uint256)) public userBalances1;
+    mapping(PoolId => address[]) public orderOwners;
+    mapping(PoolId => mapping(address => bool)) public isOrderOwner;
+
+    event OrderPlaced(
+        PoolId indexed poolId,
+        address indexed owner,
+        uint256 amount,
+        uint256 minPrice,
+        uint256 deadline,
+        bool isZeroForOne
+    );
+    event OrderCancelled(PoolId indexed poolId, address indexed owner, uint256 cancelledAmount, bool isZeroForOne);
+    event OrderPartiallyFilled(
+        PoolId indexed poolId, address indexed owner, uint256 filledAmount, uint256 remainingAmount, bool isZeroForOne
+    );
+
+    constructor(IPoolManager poolManager) BaseHook(poolManager) {}
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: true,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
 
     function beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
         external
@@ -52,7 +77,7 @@ contract UniCow is BaseHook {
     {
         PoolId poolId = key.toId();
 
-        (bool success, BeforeSwapDelta delta) = findCOW(poolId, sender, key, params);
+        (bool success, BeforeSwapDelta delta) = tryCOW(poolId, sender, key, params);
         if (success) {
             return (this.beforeSwap.selector, delta, 0);
         }
@@ -61,12 +86,13 @@ contract UniCow is BaseHook {
         return (this.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
     }
 
-    function findCOW(PoolId poolId, address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params)
+    function tryCOW(PoolId poolId, address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params)
         internal
         returns (bool, BeforeSwapDelta)
     {
         bool zeroForOne = params.zeroForOne;
         uint256 amountSpecified = uint256(params.amountSpecified < 0 ? -params.amountSpecified : params.amountSpecified);
+        (Currency inputCurrency, Currency outputCurrency, uint256 amount) = _getInputOutputAndAmount(key, params);
 
         for (uint256 i = 0; i < orderOwners[poolId].length; i++) {
             address orderOwner = orderOwners[poolId][i];
@@ -95,11 +121,23 @@ contract UniCow is BaseHook {
                 BeforeSwapDelta delta =
                     toBeforeSwapDelta(int128(-params.amountSpecified), int128(params.amountSpecified));
 
+                emit OrderPartiallyFilled(poolId, orderOwner, amountSpecified, order.amount, order.isZeroForOne);
+
                 return (true, delta);
             }
         }
 
         return (false, BeforeSwapDeltaLibrary.ZERO_DELTA);
+    }
+
+    function _getInputOutputAndAmount(PoolKey calldata key, IPoolManager.SwapParams calldata params)
+        internal
+        pure
+        returns (Currency input, Currency output, uint256 amount)
+    {
+        (input, output) = params.zeroForOne ? (key.currency0, key.currency1) : (key.currency1, key.currency0);
+
+        amount = params.amountSpecified < 0 ? uint256(-params.amountSpecified) : uint256(params.amountSpecified);
     }
 
     function placeOrder(PoolKey calldata key, uint256 minPrice, uint256 deadline, uint256 amount, bool zeroForOne)
@@ -111,14 +149,19 @@ contract UniCow is BaseHook {
         orders[poolId][msg.sender] =
             OrderObject({minPrice: minPrice, deadline: deadline, amount: amount, isZeroForOne: zeroForOne});
 
+        if (!isOrderOwner[poolId][msg.sender]) {
+            orderOwners[poolId].push(msg.sender);
+            isOrderOwner[poolId][msg.sender] = true;
+        }
+
         // Transfer tokens to the contract
         if (zeroForOne) {
-            IERC20(Currency.unwrap(key.currency0)).transferFrom(msg.sender, address(this), amount);
-            userBalances0[poolId][msg.sender] += amount;
+            key.currency0.take(poolManager, msg.sender, amount, false);
         } else {
-            IERC20(Currency.unwrap(key.currency1)).transferFrom(msg.sender, address(this), amount);
-            userBalances1[poolId][msg.sender] += amount;
+            key.currency1.take(poolManager, msg.sender, amount, false);
         }
+
+        emit OrderPlaced(poolId, msg.sender, amount, minPrice, deadline, zeroForOne);
     }
 
     function cancelExpiredOrder(PoolKey calldata key) external {
@@ -128,38 +171,23 @@ contract UniCow is BaseHook {
         require(order.amount > 0, "No active order");
         require(block.timestamp > order.deadline, "Order not expired");
 
-        // Calculate the remaining amount
-        uint256 initialBalance =
-            order.isZeroForOne ? userBalances0[poolId][msg.sender] : userBalances1[poolId][msg.sender];
-        uint256 remainingAmount = Math.min(initialBalance, order.amount);
-
-        // Refund the remaining tokens
+        // Refund the tokens
         if (order.isZeroForOne) {
-            userBalances0[poolId][msg.sender] -= remainingAmount;
-            IERC20(Currency.unwrap(key.currency0)).transfer(msg.sender, remainingAmount);
+            key.currency0.settle(poolManager, msg.sender, order.amount, false);
         } else {
-            userBalances1[poolId][msg.sender] -= remainingAmount;
-            IERC20(Currency.unwrap(key.currency1)).transfer(msg.sender, remainingAmount);
-        }
-
-        // Check if the order was partially filled
-        if (remainingAmount < order.amount) {
-            emit OrderPartiallyFilled(
-                poolId, msg.sender, order.amount - remainingAmount, remainingAmount, order.isZeroForOne
-            );
+            key.currency1.settle(poolManager, msg.sender, order.amount, false);
         }
 
         // Remove the order
         delete orders[poolId][msg.sender];
         removeOrderOwner(poolId, msg.sender);
 
-        emit OrderCancelled(poolId, msg.sender, remainingAmount, order.isZeroForOne);
+        emit OrderCancelled(poolId, msg.sender, order.amount, order.isZeroForOne);
     }
 
     function removeOrderOwner(PoolId poolId, address owner) internal {
         require(isOrderOwner[poolId][owner], "Not an order owner");
 
-        // Find the index of the owner
         uint256 index;
         for (uint256 i = 0; i < orderOwners[poolId].length; i++) {
             if (orderOwners[poolId][i] == owner) {
@@ -168,11 +196,9 @@ contract UniCow is BaseHook {
             }
         }
 
-        // Move the last element to the place of the removed one
         orderOwners[poolId][index] = orderOwners[poolId][orderOwners[poolId].length - 1];
         orderOwners[poolId].pop();
 
-        // Update the mapping
         isOrderOwner[poolId][owner] = false;
     }
 }
